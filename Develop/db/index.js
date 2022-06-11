0e2117147ab68a832ad5b63c583794380fdbae8f
Robert Routhier <robrouth92@gmail.com>
2022-06-11 10:51:07 -0700

    got some query written

---
+++ Develop/db/index.js
@@ -0,0 +1,15 @@
+// This is where the sql connection happens and export sql objects
+
+const mysql = require('mysql2');
+const util = require('util');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'employees'
+});
+
+db.query = util.promisify(db.query);
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,99 @@
+const inquirer = require('inquirer');
+const db = require('./db');
+
+
+
+const startMenu = {
+    name: "functionality",
+    message: "What would you like to do?",
+    type: "list",
+    choices: [
+        "Add a new employee",
+        "View all employees",
+        "Update an employee role",
+        "Delete an employee",
+    ]
+}
+
+const addEmployeePrompt = [{
+    name: "first_name",
+    message: "What is the employee's first name?",
+    type: "input"
+},
+{
+    name: "last_name",
+    message: "What is the employee's last name?",
+    type: "input"
+},
+{
+    name: "role_id",
+    message: "What is the employee's role id?",
+    type: "list",
+    choices: [{
+
+    }]
+},
+{
+    name: "manager_id",
+    message: "Who is the employee's manager?",
+    type: "list",
+    choices: [...managerChoices, {name: 'No Manager', value: null}]
+}
+];
+
+const showAllEmployees = () => {
+    db.query(`SELECT e1.id as EMP_ID, CONCAT(e1.first_name, e1.last_name) as Name, title as role, salary, 
+    department.name as department From employee LEFT JOIN role ON employee.role_id=role.id LEFT JOIN department ON role.department_id=department.id
+     LEFT JOIN employee e2 ON employee e1.manager_id=e2.id`).then(results => {
+        console.log('------ EMPLOYEES ------');
+        console.table(results);
+        console.log('------ END EMPLOYEES ------');
+
+        setTimeout(start, 3000);
+    })
+}
+
+const addEmployee = () => {
+    db.query('SELECT id, first_name, last_name FROM employee')
+    .then(managers => {
+        console.log(managers)
+        const managerChoices = managers.map(man => {
+            return {
+                name: `${man.first_name} ${man.last_name}`,
+                value: man.id
+    }})
+    })
+    db.query(`SELECT id, title FROM role`).then(results => {
+        console.log(results)
+        const choices = results.map(role => {
+            return {
+                name: role.title,
+                value: role.id
+    }
+}
+);
+        
+    })
+    inquirer.prompt(addEmployeePrompt).then((results) => {
+        console.log('---Results---', results);
+        db.query('INSERT INTO employee SET ?', results).then(() => {setTimeout(startMenu, 3000)
+        });
+    });
+};
+
+function start(){
+    inquirer.prompt(startMenu)
+    .then((answer) => {
+        console.log('Your choice was', answer);
+
+        switch (answer.functionality) {
+            case "Show all employees":
+                return showAllEmployees();
+            case "Add a new employee":
+                return addEmployee();
+
+
+
+        }
+    })}
+        
